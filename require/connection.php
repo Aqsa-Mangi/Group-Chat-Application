fe63f60854e38a7165f896c35cd93b9964443bbb
Aqsa Mangi <58926671+Aqsa-Mangi@users.noreply.github.com>
2023-05-01 03:28:11 +0500

    Add files via upload

--- require/connection.php
+++ require/connection.php
@@ -1,1 +1,21 @@
+<?php 
 
+	date_default_timezone_set('asia/karachi');
+	mysqli_report(FALSE);
+
+	define("hostname", "localhost");
+	define("username", "root");
+	define("password", "");
+	define("database", "group_chat_application");
+
+	$connection = mysqli_connect(hostname,username,password,database);
+
+	if(mysqli_connect_errno())   
+	{
+		echo "Error No: ".mysqli_connect_errno();
+		echo "<br>";
+		echo "Error Msg: ".mysqli_connect_error();
+		echo "<br>";
+		die("Database Connection Failed !...");	
+	}
+?>
